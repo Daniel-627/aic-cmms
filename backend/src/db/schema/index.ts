e5890bff968807ea9dd70d0aa4027d50b9d6d920
Daniel-627 <ochiengdaniel627@gmail.com>
2026-02-08 00:01:05 +0300

    enums

--- backend/src/db/schema/index.ts
+++ backend/src/db/schema/index.ts
@@ -1,3 +1,6 @@
+export * from "./enums
+";
+
 export { users } from "./users";
 export { user_roles } from "./user_roles";
 export { auth_providers } from "./auth_providers";
@@ -7,7 +10,11 @@ export { auth_sessions } from "./auth_sessions";
 export { journalEntries } from "./journalEntries";
 export { journalLines } from "./journalLines";
 export { accounts } from "./accounts";
+
+
 export { payments } from "./payments";
+
+
 export { sermons } from "./sermons";
 export { announcements } from "./announcements";
 export { events } from "./events";
